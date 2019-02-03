4325e6f87084246adf7e09869d0b51e1e52fc0c7
synesenom <enys.mones@gmail.com>
2019-02-03 16:10:08 +0100

    some fixes in webpack.config.js

---
+++ demo/node/test.js
@@ -0,0 +1,5 @@
+// let ran = require('../../dist/ran.node.min')
+var ran = require('../../dist/ran.node.min')
+
+// let normal = new ran.dist.Normal()
+console.log(ran)
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const outputDir = 'dist'
 const browserConfig = {
   target: 'web',
   mode: 'production',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, outputDir),
     filename: `${libraryName}.min.js`,
@@ -15,11 +16,26 @@ const browserConfig = {
 const nodeConfig = {
   target: 'node',
   mode: 'production',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, outputDir),
     filename: `${libraryName}.node.min.js`,
-    library: libraryName
-  }
+    library: libraryName,
+    libraryTarget: 'umd'
+  }/* ,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-es2015']
+          }
+        }
+      }
+    ]
+  } */
 }
 
 module.exports = [ browserConfig, nodeConfig ]
